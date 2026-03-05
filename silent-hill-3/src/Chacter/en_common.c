7ff4641e45f50d9ec6de2ac8a920c50c500f60be
moth dreams <mothdreams666@gmail.com>
2026-03-04 20:56:02 -0800

    sh3: split `en_common`, match 2 funcs

---
+++ silent-hill-3/src/Chacter/en_common.c
@@ -0,0 +1,162 @@
+#include "en_common.h"
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/en_common", func_0022EB70);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/en_common", func_0022EBD0);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/en_common", func_0022EC70);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/en_common", func_0022ECC0);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/en_common", func_0022ED30);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/en_common", func_0022ED90);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/en_common", func_0022EE20);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/en_common", func_0022EEB0);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/en_common", func_0022EF10);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/en_common", func_0022EF30);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/en_common", func_0022EF40);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/en_common", func_0022EFD0);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/en_common", func_0022EFE0);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/en_common", func_0022F000);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/en_common", func_0022F020);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/en_common", func_0022F040);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/en_common", func_0022F060);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/en_common", func_0022F070);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/en_common", func_0022F0E0);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/en_common", func_0022F130);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/en_common", func_0022F150);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/en_common", func_0022F170);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/en_common", func_0022F1D0);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/en_common", func_0022F1E0);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/en_common", func_0022F220);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/en_common", func_0022F270);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/en_common", func_0022F390);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/en_common", func_0022F4C0);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/en_common", func_0022F630);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/en_common", func_0022F870);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/en_common", func_0022F9A0);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/en_common", func_0022FA20);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/en_common", func_0022FA60);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/en_common", func_0022FA90);
+
+void func_0022FAC0(EnemyWork* work) {
+    switch (work->scp->kind) {
+        case CLOSER_CHARA_ID:
+            func_00230010(work);
+            return;
+        case DOUBLE_HEAD_CHARA_ID:
+            func_00215FA0(work);
+            return;
+        case NUMB_BODY_CHARA_ID:
+            func_002434C0(work);
+            return;
+        case NURSE_CHARA_ID:
+            func_0025D750(work);
+            return;
+        case INSANE_CANCER_CHARA_ID:
+            func_00266550(work);
+            return;
+        case PENDULUM_CHARA_ID:
+            func_00290F00(work);
+            return;
+        case SPLIT_WORM_CHARA_ID:
+            func_00225C20(work);
+            return;
+        case LEONARD_WOLF_CHARA_ID:
+            func_002C3B50(work);
+            return;
+        case EN_DED1_CHARA_ID:
+            func_002EE4F0(work);
+            return;
+        case MISSIONARY_CHARA_ID:
+            func_00286DC0(work);
+            return;
+        case SCRAPER_CHARA_ID:
+            func_002BA120(work);
+            return;
+        case GOD_CHARA_ID:
+            func_002D20B0(work);
+            return;
+        case SEWER_MONSTER_CHARA_ID:
+            func_002D7020(work);
+            return;
+        case CAROUSEL_HORSE_CHARA_ID:
+            func_002DFC40(work);
+            return;
+        case MEMORY_OF_ALESSA_CHARA_ID:
+            func_002E0700(work);
+            return;
+        case SLURPER_E_CHARA_ID:
+            func_00304360(work);
+            return;
+        case 0x20C: /* unknown */
+            func_00312DB0(work);
+            return;
+        case SLURPER_X_CHARA_ID:
+            func_0030DB50(work);
+    }
+}
+
+void func_0022FC20(EnemyWork* work) {
+    switch (work->scp->kind) {
+        case CLOSER_CHARA_ID:
+            func_002308F0(work);
+            return;
+        case DOUBLE_HEAD_CHARA_ID:
+            func_00216940(work);
+            return;
+        case SCRAPER_CHARA_ID:
+            func_002BA370(work);
+            return;
+        case SLURPER_E_CHARA_ID:
+            func_00304B40(work);
+            return;
+        case NUMB_BODY_CHARA_ID:
+            func_00243EF0(work);
+            return;
+        case PENDULUM_CHARA_ID:
+            func_0029ABF0(work);
+            return;
+        case NURSE_CHARA_ID:
+            func_0025E040(work);
+            return;
+        case INSANE_CANCER_CHARA_ID:
+            func_00266E50(work);
+            return;
+        case SLURPER_X_CHARA_ID:
+            func_0030DE10(work);
+    }
+}
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/en_common", func_0022FCF0);
+
+INCLUDE_ASM("asm/nonmatchings/Chacter/en_common", func_0022FE80);
---
+++ silent-hill-3/src/Chacter/en_common.h
@@ -0,0 +1,12 @@
+#ifndef EN_COMMON_H
+#define EN_COMMON_H
+
+#include "character.h"
+#include "m3_sc.h"
+
+typedef struct EnemyWork {
+    int unk_0x0;
+    SubCharacter* scp;
+} EnemyWork;
+
+#endif
